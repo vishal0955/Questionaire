1cbe041e2ef06a9902ba1012ee688c82453367f8
vishalsingh095 <vishalsingh09552@gmail.com>
2025-02-22 19:51:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import DynamicForm from './components/Form'
+
+function App() {
+ 
+  return (
+  <>
+  <DynamicForm />
+  </>
+  )
+}
+
+export default App
